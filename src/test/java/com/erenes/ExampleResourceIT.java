c44a72bb23ed80f54aafacbf9c9c88f6c5b86640
JayCeD1 <jesseeren@gmail.com>
2023-10-07 11:19:38 +0300

    initial commit

---
+++ src/test/java/com/erenes/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.erenes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
