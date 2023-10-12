bc2b3434c59cc692a7c5e1b83224069f568ca194
JayCeD1 <jesseeren@gmail.com>
2023-10-12 15:16:43 +0300

    handling error on ping method

--- src/main/java/com/erenes/service/ServerService.java
+++ src/main/java/com/erenes/service/ServerService.java
@@ -7,8 +7,10 @@ import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
 import io.smallrye.mutiny.unchecked.Unchecked;
+import io.vertx.mutiny.core.Vertx;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.UriBuilder;
 import jakarta.ws.rs.core.UriInfo;
@@ -26,6 +28,9 @@ public class ServerService {
     @Inject
     UriInfo uriInfo;
 
+    @Inject
+    Vertx vertx;
+
     public Uni<Server> create(Server server){
         return Panache.withTransaction(() -> {
             server.imageUrl = "";
@@ -35,36 +40,31 @@ public class ServerService {
     }
 
     public Uni<Server> ping(String ipAddress) {
-        Uni<Server> serverUni = serverRepo.findByIpAddress(ipAddress);
+        Uni<Server> serverUni = serverRepo.findByIpAddress(ipAddress).onItem().ifNull().failWith(new NotFoundException());
 
-        Uni<InetAddress> address = Uni.createFrom().item(Unchecked.supplier(() -> {
+        Uni<Boolean> address = Uni.createFrom().emitter(emitter -> {
             try {
-                return InetAddress.getByName(ipAddress);
+                InetAddress inetAddress = InetAddress.getByName(ipAddress);
+                boolean isReachable = inetAddress.isReachable(10000);
+                emitter.complete(isReachable);
             } catch (Exception e) {
-                throw new WebApplicationException(e);
+                emitter.fail(e);
             }
-        }));
+        });
 
         return Panache.withTransaction(() -> Uni.combine().all().unis(serverUni, address)
                 .asTuple()
-                .onItem().transform(Unchecked.function(tuple -> {
+                .onItem().transformToUni(tuple -> {
                     Server server = tuple.getItem1();
-                    InetAddress inetAddress = tuple.getItem2();
-                    try {
-                        server.status = inetAddress.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN;
-                    } catch (IOException e) {
-                        throw new IOException(e);
-                    }
-                    return server;
-                }))
+                    server.status = Boolean.TRUE.equals(tuple.getItem2()) ? Status.SERVER_UP : Status.SERVER_DOWN;
+                    return serverRepo.persist(server);
+                })
                 .onFailure(IOException.class::isInstance)
                 .recoverWithUni(() -> serverUni.onItem().transform(server -> {
                     server.status = Status.SERVER_DOWN;
                     return server;
                 }))
-                .onItem()
-                .transformToUni(server -> serverRepo.persist(server))
-                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
+//                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
         );
     }
 
